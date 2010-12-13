0d003c2c30bcfd363095713cd8fae393063f59a7
Boggie <newcastle_bp@abv.bg>
2010-12-13 11:26:04 +0200

    search i add butoni

---
+++ main.js
@@ -0,0 +1,5 @@
+(function(document,$,undefined){
+  $(function(){
+    
+  });                                                                                                              
+})(document,jQuery);
