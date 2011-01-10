15a049da50bfb3b26007fca8ec5ea15b494b0576
Boggie <newcastle_bp@abv.bg>
2011-01-10 11:25:23 +0200

    filter

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 (function(document,$,undefined){
   $(function(){
-    
+    $('#filter').bind("keyup",function(){
+      $('ul li').each(function(){
+        if($(this).text().toLowerCase().match($('#filter').val()))
+           $(this).show();
+        else
+           $(this).hide();   
+      })
+    })
   });                                                                                                              
 })(document,jQuery);
