5d5432696ae98184d43af08c921c9ae4fdc092ee
Boggie <newcastle_bp@abv.bg>
2011-01-17 11:20:02 +0200

    ne6to

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 (function(document,$,undefined){
   $(function(){
-    $('#filter').bind("keyup",function(){
+    $('#filter').bind('keyup',function(){
       $('ul li').each(function(){
         if($(this).text().toLowerCase().match($('#filter').val()))
            $(this).show();
@@ -8,5 +8,10 @@
            $(this).hide();   
       });
     });
+    
+    $('#add-new-form form').bind('submit',function(){
+      $('<li></li>').append('<h2>'+$('#add-new-form form input[name="title"]')+)
+      return false;
+    })
   });                                                                                                              
 })(document,jQuery);
